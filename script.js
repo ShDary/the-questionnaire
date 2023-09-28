0bc75c7400270931db8f6ab84996819f741caa17
Darya <shafirovadarya@gmail.com>
2023-09-28 14:11:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -36,8 +36,6 @@ async function sendFormData(formData) {
 }
 
 form.addEventListener("submit", async (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
   event.preventDefault();
 
   const formData = new FormData(form);
